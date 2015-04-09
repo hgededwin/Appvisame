19ba718b0561d37ee9815c0259d278ac765ad099
Edwin Hernandez <hgededwindaniel@gmail.com>
2015-04-09 16:17:00 -0500

    haciendo cambios en le registro

--- app/src/main/java/com/systemboy/appvisame/appvisame/RegistroActivity.java
+++ app/src/main/java/com/systemboy/appvisame/appvisame/RegistroActivity.java
@@ -1,8 +1,11 @@
 package com.systemboy.appvisame.appvisame;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -11,24 +14,26 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 
+
 public class RegistroActivity extends ActionBarActivity {
 
     String [] Areas;
     Spinner spAreas;
     ImageButton btnsatusNormal, btnStatusImportante, btnStatusUrgente;
     Button btnelegir;
+    Button btnEnviarReporte;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        btnEnviarReporte = (Button)findViewById(R.id.btnSendReport);
         btnsatusNormal = (ImageButton)findViewById(R.id.image_status_normal);
         btnStatusImportante = (ImageButton)findViewById(R.id.image_status_importante);
         btnStatusUrgente = (ImageButton)findViewById(R.id.image_status_urgente);
         btnelegir = (Button)findViewById(R.id.btn_elegir);
         btnelegir.setVisibility(View.GONE);
-
         spAreas = (Spinner)findViewById(R.id.sp_areas);
         Areas = getResources().getStringArray(R.array.array_areas);
         ArrayAdapter<String> AreasAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Areas);
@@ -38,6 +43,8 @@ public class RegistroActivity extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_my_toolbar);
         toolbar.setTitle(R.string.title_reporte);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         btnsatusNormal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,5 +86,20 @@ public class RegistroActivity extends ActionBarActivity {
             }
         });
 
+        btnEnviarReporte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistroActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        onBackPressed();
+        return true;
     }
 }
