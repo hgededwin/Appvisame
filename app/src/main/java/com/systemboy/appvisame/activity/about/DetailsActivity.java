80c57bec19c71444dbd0cfb6d6cbfec17e94f75c
Edwin Hernandez <hgededwindaniel@gmail.com>
2015-05-04 14:42:12 -0500

    Los cambios de la status bar

--- app/src/main/java/com/systemboy/appvisame/activity/about/DetailsActivity.java
+++ app/src/main/java/com/systemboy/appvisame/activity/about/DetailsActivity.java
@@ -1,7 +1,9 @@
 package com.systemboy.appvisame.activity.about;
 
+import android.annotation.TargetApi;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -22,16 +24,24 @@ public class DetailsActivity extends ActionBarActivity {
     TextView mMail;
     TextView mCity;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_acercade);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
 
 
         Toolbar toolbar_transparent = (Toolbar) findViewById(R.id.activity_my_toolbar_transparent);
         toolbar_transparent.setTitle("Contacto");
         setSupportActionBar(toolbar_transparent);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
 
 
         mContact = Integrantes.getItem(getIntent().getIntExtra(ID, 0));
