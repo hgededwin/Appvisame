741f8fdf36e3e008c6d7485e2dc50fcccf89601c
Edwin Hernández <hgededwindaniel@gmail.com>
2015-06-03 00:32:04 -0500

    bajando cambios

--- app/src/main/java/com/systemboy/appvisame/appvisame/LoginActivity.java
+++ app/src/main/java/com/systemboy/appvisame/appvisame/LoginActivity.java
@@ -56,6 +56,8 @@ public class LoginActivity extends ActionBarActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
